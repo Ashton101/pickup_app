41a54bc5925df6ed87d980ff183e656e7d188a02
Ashton <ashton@platform45.com>
2015-09-14 15:54:20 +0200

    Validation specs on pickup attributs

--- test/models/pickup_test.rb
+++ test/models/pickup_test.rb
@@ -4,11 +4,12 @@ describe "PickupTest" do
 
   def setup
     @pickup = Pickup.new
-    @pickup.valid?
+    # @pickup.valid?
   end
   describe "Validations" do
 
     it "validates when" do
+      @pickup.valid?
       refute_empty @pickup.errors[:when]
     end
 
